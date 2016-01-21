385109f7f7394c98e8fd94589fca18fb89088313
xxxnoxisxxx <pawelnaj4@gmail.com>
2016-01-21 14:36:07 +0100

    usuniete

--- qttest/main.cpp
+++ qttest/main.cpp
@@ -6,11 +6,8 @@ int main(int argc, char *argv[])
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
-    int o =0;
     //return a.exec();
 
-    //test test test
-    double o = 1;
 }
 
 
